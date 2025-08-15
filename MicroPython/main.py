641b6b6b121f85c8d808c3f330834f86dd4860ee
Thanh Tùng <electricthanhtung@gmail.com>
2025-08-15 18:36:22 +0700

    Add MicroPython project

---
+++ MicroPython/main.py
@@ -0,0 +1,29 @@
+import time
+from machine import Pin
+
+# Test for loop
+start = time.ticks_ms()
+for i in range(10_000_000):
+    pass
+end = time.ticks_ms()
+print("Test \"for loop\": ", time.ticks_diff(end, start))
+
+# Test while loop
+start = time.ticks_ms()
+j = 0
+while j < 10_000_000:
+    j += 1
+end = time.ticks_ms()
+print("Test \"while loop\": ", time.ticks_diff(end, start))
+
+# Test GPIO
+pin = Pin(2, Pin.OUT)
+while True:
+    pin.on()
+    pin.off()
+    pin.on()
+    pin.off()
+    pin.on()
+    pin.off()
+    pin.on()
+    pin.off()
